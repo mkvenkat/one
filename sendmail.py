38272f1a7c3e67008c7a32245551b83fb16b9ac1
mkvenkat <11608042+mkvenkat@users.noreply.github.com>
2019-08-16 12:57:59 -0400

    Create sendmail.py

---
+++ sendmail.py
@@ -0,0 +1,10 @@
+helo dot.com
+mail from:<user@email.com>
+rcpt to:<user@email.com>
+data
+From: user@email.com
+Subject: test mail from command line
+
+this is test number 1
+sent from drive box
+.
