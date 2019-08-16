4077d84417e9298d63c7242e55d8c89c766750e8
mkvenkat <11608042+mkvenkat@users.noreply.github.com>
2019-08-16 13:51:20 -0400

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -1,10 +1,26 @@
-helo dot.com
-mail from:<user@email.com>
-rcpt to:<user@email.com>
-data
-From: user@email.com
-Subject: test mail from command line
+#!/usr/bin/env python
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import requests
 
-this is test number 1
-sent from drive box
-.
+#body = "wait, wait"
+
+extip = requests.get('http://canhazip.com').text
+fromaddr = 'username@email.com'
+toaddrs  = 'username@email.com'
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddrs
+msg['Subject'] = "Subject"
+
+msg.attach(MIMEText(extip, 'plain'))
+#msg = MIMEText(extip)
+
+server = smtplib.SMTP('smtp.email.com',587)
+server.ehlo()
+server.starttls()
+server.login(fromaddr, "password")
+server.sendmail(fromaddr, toaddrs, msg.as_string())
+server.quit()
+
